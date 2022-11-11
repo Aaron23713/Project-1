dee0fb41d10c9375cacd4ddf7f2efa258430e856
Aaron23713 <112010276+Aaron23713@users.noreply.github.com>
2022-11-11 02:13:40 -0500

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,70 @@
+{
+    var currentCartItem = new Array();
+    var currentCartPrice = new Array();
+    
+}
+function updateCartItemBlueCheeseBurger() {
+    var image = document.getElementById('Blue Cheese Burger');
+    document.getElementsByName
+   currentCartItem.push(image);
+   for(let i = 0; i < currentCartItem.length; i++){
+    console.log(currentCartItem[i]);
+    console.log(currentCartItem.length);
+}
+}
+function updateCartItemDoubleHamburger() {
+    var image = document.getElementById('Double Hamburger');
+    document.getElementsByName
+   currentCartItem.push(image);
+   for(let i = 0; i < currentCartItem.length; i++){
+    console.log(currentCartItem[i]);
+    console.log(currentCartItem.length);
+}
+}
+function updateCartPrice() {
+    var image = document.getElementById('image');
+    document.getElementsByName
+   currentCartPrice.push(image);
+   for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    console.log(currentCartPrice.length);
+    
+}
+}
+function updateCartPrice() {
+    var image = document.getElementById('ExpensivePrice');
+    document.getElementsByName
+   currentCartPrice.push(image);
+   for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    console.log(currentCartPrice.length);
+    
+}
+}
+function updateCartItemDessert() {
+    var image = met.getElementById('nameDessert');
+    document.getElementsByName
+    sessionStorage.setItem(1,image);
+   currentCartItem.push(image);
+   for(let i = 0; i < currentCartItem.length; i++){
+    console.log(currentCartItem[i]);
+    console.log(currentCartItem.length);
+}
+}
+function updateCartPriceDessert() {
+    var image = document.getElementById('priceDessert');
+    document.getElementsByName
+   currentCartPrice.push(image);
+   for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    console.log(currentCartPrice.length);
+    
+}
+}
+function Storage(){
+    for(let i = 0; i < currentCartPrice.length; i++){
+    console.log(currentCartPrice[i]);
+    
+}
+    
+}
